74ea6ecb0f1aea600b0a6244c097e48d0546800b
Joel <joel.sim@convoy.com.hk>
2023-12-22 15:25:48 +0800

    Done prepare game object for quick tab

--- Assets/Scripts/Systems/Ui/QuickTabSystem.cs
+++ Assets/Scripts/Systems/Ui/QuickTabSystem.cs
@@ -25,8 +25,8 @@ public class QuickTabSystem : MonoBehaviour
             if (tween.IsPlaying()) return;
         }
         active = quickTabComponent.active;
-        float initialValue = active.Value ? -200f : 200f;
-        float moveValue = active.Value ? 400f : -400f;
+        float initialValue = active.Value ? -250 : 250;
+        float moveValue = active.Value ? 500f : -500f;
         quickTabComponent.rectTransform.anchoredPosition = new Vector2(quickTabComponent.rectTransform.anchoredPosition.x,
             initialValue);
         tween = quickTabComponent.rectTransform.DOBlendableLocalMoveBy(new Vector3(0, moveValue, 0), quickTabComponent.pageMoveDuration).SetEase(quickTabComponent.pageMoveEase);
