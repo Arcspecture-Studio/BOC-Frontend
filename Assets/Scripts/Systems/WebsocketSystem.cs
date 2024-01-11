7dcbafa3d008f82778418db30fb2e0958c94c37c
Joel <simkalok0914@gmail.com>
2024-01-11 09:55:36 +0800

    Updated comments

--- Assets/Scripts/Systems/WebsocketSystem.cs
+++ Assets/Scripts/Systems/WebsocketSystem.cs
@@ -137,7 +137,7 @@ public class WebsocketSystem : MonoBehaviour
             }
             else if (response.eventType.Equals(WebsocketEventTypeEnum.VERSION_CHECKING.ToString()))
             {
-                // TODO: check the body if {matched: true}
+                // TODO: now is when received this eventType VERSION_CHECKING straight means outdated, later need to check the body if the version is matching by {matched: true}
                 UnityMainThread.AddJob(() =>
                 {
                     promptComponent.ShowPrompt("NOTICE", "App version is outdated, please update your app to latest version.", () =>
