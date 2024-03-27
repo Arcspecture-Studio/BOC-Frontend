799d9a02c1397fcd55d7e6093faaec6513cc31f8
Joel <joelsim.endless@gmail.com>
2024-03-27 16:14:37 +0800

    Misc

--- Assets/Scripts/Models/API/Websocket/General/Requests/QuickOrder/WebsocketQuickOrderRequest.cs
+++ Assets/Scripts/Models/API/Websocket/General/Requests/QuickOrder/WebsocketQuickOrderRequest.cs
@@ -3,7 +3,7 @@ using System;
 namespace General
 {
     [Serializable]
-    public class WebsocketQuickOrderRequest : WebsocketOrderIdRequest
+    public class WebsocketQuickOrderRequest
     {
         public PlatformEnum platform;
         public QuickOrderSetting setting;
@@ -11,20 +11,20 @@ namespace General
         public bool isLong;
 
         public WebsocketQuickOrderRequest(PlatformEnum platform,
-        string symbol,
-        double maxLossPercentage,
-        double maxLossAmount,
-        bool weightedQuantity,
-        double quantityWeight,
-        TakeProfitTypeEnum takeProfitType,
-        double riskRewardRatio,
-        double takeProfitTrailingCallbackPercentage,
-        double entryPrice,
-        int entryTimes,
-        string atrInterval,
-        int atrLength,
-        double atrMultiplier,
-        bool isLong) : base(null)
+            string symbol,
+            double maxLossPercentage,
+            double maxLossAmount,
+            bool weightedQuantity,
+            double quantityWeight,
+            TakeProfitTypeEnum takeProfitType,
+            double riskRewardRatio,
+            double takeProfitTrailingCallbackPercentage,
+            double entryPrice,
+            int entryTimes,
+            string atrInterval,
+            int atrLength,
+            double atrMultiplier,
+            bool isLong)
         {
             this.platform = platform;
             this.setting = new(symbol, maxLossPercentage, maxLossAmount, weightedQuantity, quantityWeight, takeProfitType, riskRewardRatio, takeProfitTrailingCallbackPercentage, entryTimes, atrInterval, atrLength, atrMultiplier);
