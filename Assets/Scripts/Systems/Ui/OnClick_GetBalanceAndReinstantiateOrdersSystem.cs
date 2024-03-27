edcb22fd67c2e4621e0ba00394b78e6a26fa49dd
Joel <joelsim.endless@gmail.com>
2024-03-27 12:02:39 +0800

    Misc

--- Assets/Scripts/Systems/Ui/OnClick_GetBalanceAndReinstantiateOrdersSystem.cs
+++ Assets/Scripts/Systems/Ui/OnClick_GetBalanceAndReinstantiateOrdersSystem.cs
@@ -5,16 +5,12 @@ public class OnClick_GetBalanceAndReinstantiateOrdersSystem : MonoBehaviour
 {
     PlatformComponent platformComponent;
     HideAllPanelComponent hideAllPanelComponent;
-    TradingBotComponent tradingBotComponent;
-    MiniPromptComponent miniPromptComponent;
     Button button;
 
     void Start()
     {
         platformComponent = GlobalComponent.instance.platformComponent;
         hideAllPanelComponent = GlobalComponent.instance.hideAllPanelComponent;
-        tradingBotComponent = GlobalComponent.instance.tradingBotComponent;
-        miniPromptComponent = GlobalComponent.instance.miniPromptComponent;
         button = GetComponent<Button>();
 
         button.onClick.AddListener(() =>
@@ -22,9 +18,7 @@ public class OnClick_GetBalanceAndReinstantiateOrdersSystem : MonoBehaviour
             hideAllPanelComponent.hideNow = "true";
 
             platformComponent.getBalance = true;
-            // TODO: get runtime data
-            tradingBotComponent.getTradingBots = true;
-            // miniPromptComponent.message = "Refreshed";
+            // TODO: get runtime data (orders, quickOrders, tradingBots)
         });
     }
 }
