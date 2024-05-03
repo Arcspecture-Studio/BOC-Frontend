91a931fb9e673808ba4ee7268870e42911dd96c9
Joel <joelsim.endless@gmail.com>
2024-05-03 14:29:05 +0800

    Done Info panel data in quick order

--- Assets/Scripts/Components/Ui/QuickOrderDataRowComponent.cs
+++ Assets/Scripts/Components/Ui/QuickOrderDataRowComponent.cs
@@ -5,11 +5,19 @@ using UnityEngine.UI;
 public class QuickOrderDataRowComponent : MonoBehaviour
 {
     [Header("Reference")]
-    public TMP_Text orderIdText;
+    public TMP_Text symbolText;
     public TMP_Text positionSideText;
     public TMP_Text entryPriceText;
+    public Button infoButton;
     public Button closeButton;
+    public GameObject infoPanel;
+    public Transform infoPanelContent;
+    public GameObject infoPanelData;
 
     [Header("Config")]
     public string orderId;
+    public Preference setting;
+
+    [Header("Runtime")]
+    public bool infoPanelInstantiated = false;
 }
