8faf5df9862836188949127465ed00255b94d7e2
Joel <joelsim.endless@gmail.com>
2024-05-27 09:21:55 +0800

    Updated server ip address

--- Assets/Scripts/Configs/WebsocketConfig.cs
+++ Assets/Scripts/Configs/WebsocketConfig.cs
@@ -1,7 +1,7 @@
 public static class WebsocketConfig
 {
     public static readonly string GENERAL_HOST_LOCAL = "ws://localhost";
-    public static readonly string GENERAL_HOST = "ws://13.214.62.217";
+    public static readonly string GENERAL_HOST = "ws://54.255.11.230";
     public static readonly string GENERAL_PORT = "3000";
     public static readonly string GENERAL_PORT_TEST = "3001";
     public static readonly string GENERAL_PORT_PRODUCTION = "3002";
