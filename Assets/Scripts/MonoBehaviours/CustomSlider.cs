ba8071f82b7261072a581fee9baf3fce58f98eb2
Joel <simkalok0914@gmail.com>
2024-05-28 19:57:35 +0800

    [WIP] Make a slider component that will auto sync slider and input #2

--- Assets/Scripts/MonoBehaviours/CustomSlider.cs
+++ Assets/Scripts/MonoBehaviours/CustomSlider.cs
@@ -20,28 +20,40 @@ public class CustomSlider : MonoBehaviour
     [HideInInspector] public UnityEvent<float> onSliderMove = new();
     [HideInInspector] public UnityEvent onSliderUp = new();
     [HideInInspector] public UnityEvent<string> onInputSubmit = new();
+    int precision;
 
     void Start()
     {
-        slider.onValueChanged.AddListener(onSliderMove.Invoke);
-        input.onSubmit.AddListener(onInputSubmit.Invoke);
+        slider.onValueChanged.AddListener(OnSliderMove);
+        input.onSubmit.AddListener(OnInputSubmit);
         EventTrigger.Entry pointerUpEvent = new() { eventID = EventTriggerType.PointerUp };
         pointerUpEvent.callback.AddListener(eventData => onSliderUp.Invoke());
         sliderTrigger.triggers.Add(pointerUpEvent);
     }
 
-    public void SetRange(float min, float max)
+    public void SetRangeAndPrecision(float min, float max, int precision = 2)
     {
         slider.minValue = min;
         slider.maxValue = max;
         minText.text = min.ToString() + postfix;
         maxText.text = max.ToString() + postfix;
+        this.precision = precision;
     }
-    public void SetValue(double value, int precision = 2)
+    void SetValue(double value)
     {
         double parsedValue = Math.Min(Math.Max(value, slider.minValue), slider.maxValue);
         double roundedValue = Utils.RoundNDecimal(parsedValue, precision);
         slider.value = (float)roundedValue;
         input.text = roundedValue.ToString();
+    }
+    void OnSliderMove(float value)
+    {
+        SetValue(value);
+        onSliderMove.Invoke(value);
+    }
+    void OnInputSubmit(string value)
+    {
+        SetValue(double.Parse(value));
+        onInputSubmit.Invoke(value);
     }
 }
