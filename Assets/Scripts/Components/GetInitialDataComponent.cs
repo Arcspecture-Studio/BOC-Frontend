575cb216f0e7f78803f0b186938b3c26d96809d4
Joel <joelsim.endless@gmail.com>
2024-06-24 09:41:43 +0800

    Updated initial data and runtime data usages and write comments regarding those usage

--- Assets/Scripts/Components/GetInitialDataComponent.cs
+++ Assets/Scripts/Components/GetInitialDataComponent.cs
@@ -3,6 +3,14 @@ using UnityEngine.Events;
 
 public class GetInitialDataComponent : MonoBehaviour
 {
+    /* Current usages
+     - on login and get token
+     - on add new platform
+     - on remove a platform
+     - on remove a profile
+     - on change platform in a profile
+     - on change default profile
+    */
     public bool getInitialData
     {
         set { onChange_getInitialData.Invoke(); }
