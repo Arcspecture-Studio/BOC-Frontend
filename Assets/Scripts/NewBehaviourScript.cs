9a23bea2cdd5b6983db5ab540240fa77d63ac7ad
Joel <simkalok0914@gmail.com>
2024-10-16 15:52:55 +0800

    Test env var

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool a;
+    public TMP_Text text;
+
+    void Start()
+    {
+        if (a)
+        {
+            text.text = Environment.GetEnvironmentVariable("WEBSOCKET_SERVER_HOST");
+        }
+        else
+        {
+            text.text = Environment.GetEnvironmentVariable("ENCRYPTION_ACCESS_TOKEN_32");
+        }
+    }
+}
