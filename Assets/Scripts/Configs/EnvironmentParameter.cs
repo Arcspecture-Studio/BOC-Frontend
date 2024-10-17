9af95cbe2c030e61d8ca45c609dbffdce185a873
Joel <simkalok0914@gmail.com>
2024-10-17 17:19:18 +0800

    Test env var #4

---
+++ Assets/Scripts/Configs/EnvironmentParameter.cs
@@ -0,0 +1,6 @@
+
+public static class EnvironmentParameter
+{
+    public static readonly string ENCRYPTION_ACCESS_TOKEN_32 = null;
+    public static readonly string WEBSOCKET_SERVER_HOST = null;
+}
