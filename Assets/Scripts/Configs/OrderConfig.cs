ec19a3d9e6303ccbe5448b1723fc46cf2ea7249f
Joel <simkalok0914@gmail.com>
2024-11-29 15:14:42 +0800

    Fixed slider set range and precision after set value issue

--- Assets/Scripts/Configs/OrderConfig.cs
+++ Assets/Scripts/Configs/OrderConfig.cs
@@ -8,6 +8,8 @@ public static class OrderConfig
     public static readonly Color DISPLAY_COLOR_ORANGE = new(1, 150 / 255f, 0);
     public static readonly Color DISPLAY_COLOR_YELLOW = new(150 / 255f, 150 / 255f, 0);
     public static readonly Color DISPLAY_COLOR_CYAN = new(0, 150 / 255f, 150 / 255f);
-    public static readonly float marginWeightDistributionValueMin = -5;
-    public static readonly float marginWeightDistributionValueMax = 5;
+    public static readonly float MARGIN_WEIGHT_DISTRIBUTION_VALUE_MIN = -5;
+    public static readonly float MARGIN_WEIGHT_DISTRIBUTION_VALUE_MAX = 5;
+    public static readonly float TRAILING_MIN_PERCENTAGE = 0.1f;
+    public static readonly float TRAILING_MAX_PERCENTAGE = 10f;
 }
--- Assets/Scripts/Systems/Ui/Slider_TakeProfitQuantityPercentageSystem.cs
+++ Assets/Scripts/Systems/Ui/Slider_TakeProfitQuantityPercentageSystem.cs
@@ -4,15 +4,12 @@ public class Slider_TakeProfitQuantityPercentageSystem : MonoBehaviour
 {
     [SerializeField] OrderPageComponent orderPageComponent;
     [SerializeField] SettingPageComponent settingPageComponent;
-    ProfileComponent profileComponent;
 
     float min = 1;
     float max = 100;
 
-    void Start()
+    void Awake()
     {
-        profileComponent = GlobalComponent.instance.profileComponent;
-
         ForOrderPageComponent();
         ForSettingPageComponent();
     }
@@ -30,12 +27,12 @@ public class Slider_TakeProfitQuantityPercentageSystem : MonoBehaviour
         settingPageComponent.takeProfitQuantityPercentageCustomSlider.SetRangeAndPrecision(min, max);
 
         settingPageComponent.takeProfitQuantityPercentageCustomSlider.onSliderMove.AddListener(value =>
-        profileComponent.activeProfile.preference.order.takeProfitQuantityPercentage = value);
+        GlobalComponent.instance.profileComponent.activeProfile.preference.order.takeProfitQuantityPercentage = value);
         settingPageComponent.takeProfitQuantityPercentageCustomSlider.onSliderUp.AddListener(() =>
         settingPageComponent.updatePreferenceToServer = true);
         settingPageComponent.takeProfitQuantityPercentageCustomSlider.onInputSubmit.AddListener(value =>
         {
-            profileComponent.activeProfile.preference.order.takeProfitQuantityPercentage = value;
+            GlobalComponent.instance.profileComponent.activeProfile.preference.order.takeProfitQuantityPercentage = value;
             settingPageComponent.updatePreferenceToServer = true;
         });
     }
